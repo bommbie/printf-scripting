e89c5222f099e1defa319d48a0d0abaa4943e2c6
maxim <maxim.bassow@tfb.eu>
2026-03-06 10:43:57 +0000

    Prüfe auf Parameter

--- printf.sh
+++ printf.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/sh
 
-printf "Hallo Zusammen!%s\n"
-printf "Heute ist der: \e[31m$(date)%s\n"
+if [ $# -eq 0 ]; then
+	printf "Es wurde kein Parameter übergeben!\n"
+	printf "Verwendung %s <dein Parameter>\n" "$0"
+	exit 1
+fi
+
+programname="$0"
+parameter="$1"
+
+printf "Das Programm heißt: %s\n" "$programname"
+printf "Als Parameter wurde %s übergeben\n" "$parameter"
